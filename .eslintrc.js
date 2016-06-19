d2ef36355b0e1d9bebdb4642122c5dc406ef3e5d
Whyme Lyu <callme5long@gmail.com>
2016-06-19 23:12:15 +0800

    Enable ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "node": true
+  },
+  "extends": "eslint-config-airbnb",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "comma-dangle": [
+      "error",
+      "always-multiline"
+    ],
+    "comma-style": 0,
+    "no-console": 0,
+    "one-var": 0,
+    "prefer-rest-params": 0,
+    "no-underscore-dangle": 0,
+    "prefer-arrow-callback": 0,
+    "space-before-function-paren": 0,
+    "func-names": 0,
+    "no-param-reassign": 0,
+    "no-unused-vars": [2, {"args": "none"}],
+  }
+}
